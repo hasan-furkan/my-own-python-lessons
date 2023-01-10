fa12a03d50709326515e82706ee3f441f2e75d41
hasan-furkan <hsnfrkn32@gmail.com>
2023-01-10 14:09:04 +0300

    dunder method

--- lesson5.py
+++ lesson5.py
@@ -18,12 +18,29 @@ class Personel:
     def __str__(self):
         return f"{self.get_full_name()} -- {self.mail}"
     
+    def __add__(self, other):
+        if isinstance(self, Personel):
+            return self.price + other.price
 
-# repr ve str arasinda basit bir fark var. repr daha cok kod yazan kisiler icin. bu kodu 2000. satirda cagirdigi zaman anlayabilmesi icin. str ise son kullanicilar icin hazirlanmis olan DUNDER koddur.
+    def __len__(self):
+        return len(self.get_full_name())
+ 
+ 
+"""
+repr daha cok kod yazan kisiler icin. bu kodu 2000. satirda cagirdigi zaman anlayabilmesi icin.
+repr ve str arasinda basit bir fark var. 
+str ise son kullanicilar icin hazirlanmis olan DUNDER koddur.
+""" 
 
+        
 per_1 = Personel("Hasan", "furkan", 40000)
 per_2 = Personel("Hasan", "koprulu", 12000)
 
-print(per_1)
-print(str(per_1))
-print(repr(per_1))
+# print(per_1)
+# print(str(per_1))
+# print(repr(per_1))
+
+# print(per_1.price + per_2. price)
+print(per_1 + per_2)
+# print(per_1.name, per_1.surname)
+# print(len(per_1))
