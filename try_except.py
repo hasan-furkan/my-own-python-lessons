187cfb48da3aaeb9145bbd0b33073bda383de597
hasan-furkan <hsnfrkn32@gmail.com>
2024-05-07 01:32:03 +0200

    add try except file raise as

--- try_except.py
+++ try_except.py
@@ -22,14 +22,46 @@
 # ValueError: invalid literal for int() with base 10: 'dsfa' or ZeroDivisionError: division by zero
 # print(number1, "/", number2, "=", number1 / number2)
 
-number3 = input("number3: ")
-number4 = input("number4: ")
+# number3 = input("number3: ")
+# number4 = input("number4: ")
+
+# try:
+#     number3 = int(number3)
+#     number4 = int(number4)
+#     print(number3, "/", number4, "=", number3 / number4)
+# except ValueError:
+#     print("Please enter a number")
+# except ZeroDivisionError:
+#     print("Cannot divide by 0")
+
+
+# Except as
+
+# try:
+#     number3 = int(number3)
+#     number4 = int(number4)
+#     print(number3, "/", number4, "=", number3 / number4)
+# except ValueError as e:
+#     print("e", e)
+
+
+# finally
 
 try:
-    number3 = int(number3)
-    number4 = int(number4)
+    file = open("filename", "r")
+except IOError as e:
+    print("e", e)
+finally:
+    file.close()
+
+
+# raise
+
+try:
+    number3 = int(input("number3: "))
+    number4 = int(input("number4: "))
+    if number4 == 23:
+        raise ZeroDivisionError("23 is not allowed as a denominator")
     print(number3, "/", number4, "=", number3 / number4)
-except ValueError:
-    print("Please enter a number")
-except ZeroDivisionError:
-    print("Cannot divide by 0")
+except (ValueError, ZeroDivisionError) as e:
+    print("e", e)
